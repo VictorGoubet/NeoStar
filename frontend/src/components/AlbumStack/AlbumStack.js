e47400ff7eec6dd7352e8df90ccdc3ff94494a3e
Victor Goubet <victor.goubet@edu.devinci.fr>
2022-02-04 16:56:06 +0100

    build for linux

--- frontend/src/components/AlbumStack/AlbumStack.js
+++ frontend/src/components/AlbumStack/AlbumStack.js
@@ -8,7 +8,7 @@ class AlbumStack extends React.Component {
   state = {
     socket:null,
     id : '',
-    root:process.env.NODE_ENV === "development"?"http://localhost:5000":"https://neostar.herokuapp.com:5000"
+    root:process.env.NODE_ENV === "development"?"http://localhost:5000":"https://neostar.herokuapp.com"
   }
 
   del_album = event => {
